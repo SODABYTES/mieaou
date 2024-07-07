a328f7868573fffe1c4daf5034bd4e5075f007ac
SODABYTES <146698058+SODABYTES@users.noreply.github.com>
2024-07-07 12:22:44 -0400

    updated camera control
    MREOOOOUW

--- scene/entity/peeper/peeper.gd
+++ scene/entity/peeper/peeper.gd
@@ -3,9 +3,12 @@ extends Node3D
 
 var cameraSpeed:float = 3
 var zoomSpeed:float = 3
+var moveSpeed:float = 2
 
 var inputAxisL:Vector2 = Vector2(0,0)
 var inputAxisR:Vector2 = Vector2(0,0)
+
+var inputPad:Vector2 = Vector2(0,0)
 
 var zoomIn:bool = false
 var zoomOut:bool = false
@@ -19,14 +22,21 @@ func _physics_process(delta):
 	rotation.x = clampf(rotation.x, -PI / 2, PI / 2)
 	
 	if zoomIn:
-		camera.position.z -= zoomSpeed * delta
+		position.y -= zoomSpeed * delta
 	if zoomOut:
-		camera.position.z += zoomSpeed * delta
-	pass
+		position.y += zoomSpeed * delta
+	
+	position.x += (inputPad.x * cos(rotation.y)) * moveSpeed * delta
+	position.z += (inputPad.x * -sin(rotation.y)) * moveSpeed * delta
+	
+	position.z += (inputPad.y * cos(rotation.y)) * moveSpeed * delta
+	position.x += (inputPad.y * sin(rotation.y)) * moveSpeed * delta
 
 func getInput():
 	inputAxisL = Vector2(Input.get_joy_axis(0, JOY_AXIS_LEFT_X), Input.get_joy_axis(0, JOY_AXIS_LEFT_Y))
 	inputAxisR = Vector2(Input.get_joy_axis(0, JOY_AXIS_RIGHT_X), Input.get_joy_axis(0, JOY_AXIS_RIGHT_Y))
+	inputPad.x = int(Input.is_joy_button_pressed(0, JOY_BUTTON_DPAD_RIGHT)) - int(Input.is_joy_button_pressed(0, JOY_BUTTON_DPAD_LEFT))
+	inputPad.y = int(Input.is_joy_button_pressed(0, JOY_BUTTON_DPAD_DOWN)) - int(Input.is_joy_button_pressed(0, JOY_BUTTON_DPAD_UP))
 	if Input.get_joy_axis(0, JOY_AXIS_TRIGGER_RIGHT) > 0.5:
 		zoomIn = true
 	else:
