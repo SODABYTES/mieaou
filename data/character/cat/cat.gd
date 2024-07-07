abd3312ff2245c7c8f1fbf907a3c2ca0d7997d70
SODABYTES <146698058+SODABYTES@users.noreply.github.com>
2024-07-07 13:45:33 -0400

    add animation to cat
    muowe mwmero

--- data/character/cat/cat.gd
+++ data/character/cat/cat.gd
@@ -1,31 +1,39 @@
 extends Node3D
+@onready var model = $cat
 @onready var audio = $AudioStreamPlayer3D
 var wasSingPressed:bool = false
 
 var buttonSing:bool = false
-var pitchShift:float = 0
+var stick:Vector2 = Vector2(0,0)
+var headTilt
 
-# Called when the node enters the scene tree for the first time.
+var jawTarget:float = -1.2
+var jawCurrent:float = jawTarget
+
 func _ready():
-	pass # Replace with function body.
+	jawCurrent = jawTarget
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	getInput()
 	if buttonSing:
+		jawTarget = -PI / 2
 		if !wasSingPressed:
 			audio.play()
 		wasSingPressed = true
 	else:
+		jawTarget = -1.2
 		audio.stop()
 		wasSingPressed = false
-	if pitchShift <= 0:
-		audio.pitch_scale = 1 - pitchShift
+	if stick.y <= 0:
+		audio.pitch_scale = 1 - stick.y
 	else:
-		audio.pitch_scale = 1 - (pitchShift / 2)
+		audio.pitch_scale = 1 - (stick.y / 2)
+	
+	jawCurrent = lerpf(jawCurrent, jawTarget, 32 * delta)
+	
+	model.get_node("Armature").get_node("Skeleton3D").set_bone_pose_rotation(1, Quaternion(Vector3.LEFT, jawCurrent))
+	model.get_node("Armature").get_node("Skeleton3D").set_bone_pose_rotation(0, Quaternion(Vector3.LEFT, (-stick.y / 2) - (PI / 2)) + Quaternion(Vector3.UP, stick.x))
 
 func getInput():
 	buttonSing = Input.get_joy_axis(0, JOY_AXIS_TRIGGER_LEFT)
-	
-	pitchShift = Input.get_joy_axis(0, JOY_AXIS_LEFT_Y)
+	stick = Vector2(Input.get_joy_axis(0, JOY_AXIS_LEFT_X),Input.get_joy_axis(0, JOY_AXIS_LEFT_Y))
