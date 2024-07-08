a8c0f0c963e802b0444733cdf68f347cf7d7fd29
SODABYTES <146698058+SODABYTES@users.noreply.github.com>
2024-07-08 12:59:26 -0400

    add artwork to no controller screen
    meouwouwouowe

--- scene/screen/noController/noController.gd
+++ scene/screen/noController/noController.gd
@@ -1,5 +1,5 @@
 extends Node
-@onready var cameraPivot = $CameraPivot
+@onready var cameraPivot = $CanvasLayer/SubViewportContainer/SubViewport/CameraPivot
 
 var timePassed:float = 0
 
@@ -7,3 +7,5 @@ func _process(delta):
 	timePassed += 1 * delta
 	cameraPivot.rotation.x = sin(timePassed * 3) / 4
 	cameraPivot.rotation.y = cos(timePassed * 3) / 4
+	if Input.get_connected_joypads().size() > 0:
+		get_tree().change_scene_to_file("res://scene/test/test.tscn")
